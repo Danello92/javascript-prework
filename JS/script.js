c2c082cb14291947ac3f5e298e028a9c256366d8
Piotr Danisz <piotrdanisz123@gmail.com>
2020-06-28 16:30:51 +0200

    add move PC

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,4 @@
 	
-let computerMove = `kamień`;
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-let playerMove = 'papier'
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -12,13 +6,16 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
-  playerMove = 'kamień';
+if(playerInput == '1')  
+{
+ playerMove = 'kamień';
 }
-if else (playerInput = '2'){
-    playerMove='papier';
+else if(playerInput == '2') 
+{
+     playerMove = 'papier';
 }
-else (playerInput = '3') {
-    playerMove='nożyce';
+else (playerInput == '3') 
+{ 
+    playerMove = 'nożyce.';
 }
 printMessage('Twój ruch to: ' + playerMove);
